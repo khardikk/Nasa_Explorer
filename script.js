779e8533523ebf6a9f7807a46fab0a19383d4e85
Hardikk <kambojpushkal@gmail.com>
2023-09-27 00:05:02 +0530

    feat: added the date functionality

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.querySelector('button.btn.btn-primary').addEventListener('click', getFetch);
 
-async function getFetch(){
+async function getFetch() {
+    const inputdate = document.querySelector('input').value; // Get the selected date
     
-    // const choice = document.querySelector('input').value;
-    const url = 'https://api.nasa.gov/planetary/apod?api_key=oXkGEcmbGaIGmArTreN2hKNGVLnrUqT4T0p22agC';
+    // Construct the URL with the selected date
+    const url = `https://api.nasa.gov/planetary/apod?api_key=oXkGEcmbGaIGmArTreN2hKNGVLnrUqT4T0p22agC&date=${inputdate}`;
+    
     const button = document.querySelector('button.btn.btn-primary');
     button.disabled = true; // Disable the button during fetching
     
-    try{
+    try {
         const response = await fetch(url);
         const data = await response.json();
 
@@ -22,15 +24,11 @@ async function getFetch(){
 
         document.getElementById('explaination').innerText = data.explanation;
         document.querySelector('h2').innerText = data.title;
-        document.querySelector('h4').innerText =  data.date;
-        document.getElementById('copyright').innerText =  data.date + ' ';
+        document.querySelector('h4').innerText = data.date;
+        document.getElementById('copyright').innerText = data.date + ' ';
     } catch (err) {
         console.error(`Error: ${err.message}`);
     } finally {
         button.disabled = false; // Re-enable the button after fetching
     }
 }
-
-
-
-
